27c0ddf68ba54d80bfd040aa0509f07112814326
Aniket Kotal <44394628+mightykillrr@users.noreply.github.com>
2022-06-26 02:29:26 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      p_text: "#b1b1b1",
+      head_text: "#eaeaea",
+      navbar_back: "rgba(17, 17, 17, 0.8)",
+      action_back: "#242424",
+      action_border: "#363636",
+      action_text: "#d9d9d9",
+      btn_back: "#2e2e2e",
+      btn_text: "#f6f6f6",
+      link_text_hover: "#8153E2",
+      link_border_hover: "rgba(129, 83, 226, 0.4)",
+      link_border: "#6c6c6c",
+      link: "#d9d9d9",
+      background: "#111111",
+    },
+    fontFamily: {
+      logo: "Southam",
+      theme: "Source Code Pro",
+    },
+  },
+  plugins: [],
+};
