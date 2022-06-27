c69daef2a9cc599053cdb58ece02c6a82fa080d0
Aniket Kotal <44394628+mightykillrr@users.noreply.github.com>
2022-06-27 20:26:56 +0530

    final responsive changes

--- src/views/TheAbout.vue
+++ src/views/TheAbout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col gap-8">
     <p
-      class="text-sm md:text-base md:leading-relaxed slg:text-lg lg:leading-loose"
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
       I'm Aniket Kotal, an India based Vue.js developer/Student @ Parul
       University, with never-stopping love for learning new tech. I am
@@ -10,7 +10,7 @@
       interfaces with programming languages.
     </p>
     <p
-      class="text-sm md:text-base md:leading-relaxed slg:text-lg lg:leading-loose"
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
       I'm currently working on
       <LinkBtn link="https://scarletdynasty.vercel.app">Scarlet Dynasty</LinkBtn
@@ -21,7 +21,7 @@
       polish my web-development skills.
     </p>
     <p
-      class="text-sm md:text-base md:leading-relaxed slg:text-lg lg:leading-loose"
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
       I started learning about web-development in my early high-school, where I
       was introduced to HTML and CSS. I quickly started writing small webpages
@@ -32,7 +32,7 @@
       TailwindCSS, Express and more.
     </p>
     <p
-      class="text-sm md:text-base md:leading-relaxed slg:text-lg lg:leading-loose"
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
       You can find my projects on my
       <LinkBtn link="https://github.com/mightykillrr">Github</LinkBtn> or on my
@@ -40,7 +40,7 @@
       portfolio page but all my recent one's are on github.
     </p>
     <p
-      class="text-sm md:text-base md:leading-relaxed slg:text-lg lg:leading-loose"
+      class="text-sm md:text-base leading-relaxed slg:text-lg lg:leading-loose"
     >
       Outside of my worklife, I love to play Valorant and CS:GO, listening to
       Em. on Spotify, or just hanging out on Discord with my friends. You can
@@ -52,17 +52,21 @@
     <hr class="bg-squgily bg-repeat border-0 h-[6px] opacity-25 my-6" />
     <div class="flex flex-col gap-5">
       <h3 class="font-theme_bold text-head_text text-3xl">Work</h3>
-      <div class="flex flex-col gap-4 py-4">
+      <div class="flex flex-col gap-8 py-4">
         <a
-          class="flex flex-nowrap justify-between items-start gap-6"
+          class="flex flex-col gap-1 sm:flex-row flex-nowrap justify-between items-start sm:gap-6"
           v-for="work in works"
           :key="work.id"
           :href="work.link"
         >
           <span class="font-theme_bold text-head_text">{{ work.name }}</span>
           <span class="flex-[1_1_auto]"></span>
-          <span class="text-center">{{ work.position }}</span>
-          <span>{{ work.from }} - {{ work.to }}</span>
+          <span class="sm:text-right text-sm sm:text-base">{{
+            work.position
+          }}</span>
+          <span class="text-sm sm:text-base"
+            >{{ work.from }} - {{ work.to }}</span
+          >
         </a>
       </div>
     </div>
