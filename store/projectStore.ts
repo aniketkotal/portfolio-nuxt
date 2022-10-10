6bf23074f0dc3e7de581deddc9127c0f32eba035
Aniket Kotal <aniket1010104@gmail.com>
2022-10-10 06:47:33 +0530

    Compressed Images + Updated Asset Handling
    - Fixed Image paths being broken

--- store/projectStore.ts
+++ store/projectStore.ts
@@ -1,12 +1,15 @@
 import {defineStore} from "pinia";
 import {ProjectStore} from "../types/typings";
 
+// @ts-ignore
+const images: Record<string, string> = import.meta.glob("../assets/featured/*.{png,jpg}", {as: 'url', eager: true});
+
 export const useProjectStore = defineStore<string, ProjectStore>("projectStore", () => {
     const projects = [
         {
             id: 6,
             name: "Hanako - Discord Bot",
-            image: "../assets/featured/hanako.jpg",
+            image: images["../assets/featured/hanako.jpg"],
             description:
                 "A multiple-purpose Discord Bot with a focus on fun and utility commands. Made this bot to help me with my Discord server.",
             tech: ["TypeScript", "Discord.js", "Node.js"],
@@ -16,7 +19,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 3,
             name: "Stust - Fictional Card Designers",
-            image: "../assets/featured/stust.png",
+            image: images["../assets/featured/stust.png"],
             description:
                 "Stust is a made-up company which designs minimalistic card designs with high quality. This was a website made to practice Vue.js",
             tech: ["Vue.js", "TailwindCSS"],
@@ -26,7 +29,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 4,
             name: "Mikey Wazooski",
-            image: "../assets/featured/mikey.jpg",
+            image: images["../assets/featured/mikey.jpg"],
             description:
                 "A feature-rich Discord bot I made to help me with my server and give action commands with great GIFs.",
             tech: ["JS", "Node.js", "MongoDB", "Heroku"],
@@ -35,7 +38,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 1,
             name: "Scarlet Dynasty",
-            image: "../assets/featured/scarlet_dyn.png",
+            image: images["../assets/featured/scarlet_dyn.png"],
             description:
                 "I made this website for my friend's online store. Scarlet Dynasty is a team creating content for Minecraft Marketplace.",
             tech: ["Vue.js", "Pinia", "TailwindCSS"],
@@ -45,7 +48,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 2,
             name: "My Old Portfolio",
-            image: "../assets/featured/portfolioprev.png",
+            image: images["../assets/featured/portfolioprev.png"],
             description:
                 "This is my first portfolio page. Before this one, I used it to showcase my projects.",
             tech: ["Vue.js", "TailwindCSS"],
@@ -54,7 +57,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 5,
             name: "Advice Generator",
-            image: "../assets/featured/advice-gen.png",
+            image: images["../assets/featured/advice-gen.png"],
             description:
                 "A very simple Advice Generator, which gives advice with a click!",
             tech: ["HTML", "CSS", "JS", "REST APIs"],
@@ -66,7 +69,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 1,
             name: "Text Case Changer",
-            image: "../assets/featured/case_changer.jpg",
+            image: images["../assets/featured/case_changer.jpg"],
             description:
                 "A simple text case modifier to change case of text as need.",
             tech: ["Python", "Pandas"],
@@ -75,7 +78,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 2,
             name: "Tic Tac Toe",
-            image: "../assets/featured/tic_tac_toe.jpg",
+            image: images["../assets/featured/tic_tac_toe.jpg"],
             description:
                 "A game I made as a school project which I am very proud of.",
             tech: ["Python", "Pandas"],
@@ -84,7 +87,7 @@ export const useProjectStore = defineStore<string, ProjectStore>("projectStore",
         {
             id: 3,
             name: "Advanced Calculator",
-            image: "../assets/featured/calc.jpg",
+            image: images["../assets/featured/calc.jpg"],
             description:
                 "An advanced calculator and my first ever HUGE Python project.",
             tech: ["Python", "MathLib"],
